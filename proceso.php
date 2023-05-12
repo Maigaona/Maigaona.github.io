a8d71284965e0365caf52548bb0e87e28e695337
Maigaona <133295172+Maigaona@users.noreply.github.com>
2023-05-11 20:19:33 -0600

    inicial commit

---
+++ proceso.php
@@ -0,0 +1,25 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+echo $nombre ; 
+
+echo "<br/>";
+
+$correo = $_POST['correo'];
+
+echo $correo ;
+
+echo "<br/>";
+
+$telefono = $_POST['telefono'];
+
+echo $telefono ;
+echo "<br/>";
+
+$dimealgo = $_POST['dimealgo'];
+
+echo $dimealgo ;
+
+
+?>
